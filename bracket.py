3b893a9ba01bf8b2cd2ac9886e29aee99c9b868d
YouHengZhou <youhengzhou@cmail.carleton.ca>
2021-07-03 17:34:26 -0400

    bracket things

---
+++ bracket.py
@@ -0,0 +1,16 @@
+import random
+from engine import engine
+
+bracke123 = {
+    "char1": 0
+}
+
+bracket = ["char1","char2"]
+
+print(engine.random_num(5))
+
+for i in range(5):
+    print(i)
+
+for i in (bracket):
+    print(i)
